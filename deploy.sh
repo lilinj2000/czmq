b51ecaed32509853f4a3feba9d567b64f9b678ca
lilinj2000 <lilinj2000@gmail.com>
2017-09-13 16:31:16 +0800

    distribution, support os version.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 ./autogen.sh
 
-home_3rd=~/3rd
+node=`uname -n`
+home_3rd=~/3rd/${node}
 
 ./configure --prefix=$home_3rd/czmq libzmq_CFLAGS="-I $home_3rd/libzmq/include" libzmq_LIBS=$home_3rd/libzmq/lib/libzmq.la
 
